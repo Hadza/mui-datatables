f783653df263816a156ac8d017933d8e57d41b11
Hadza <eddieisaac@gmail.com>
2019-10-31 10:19:11 -0700

    updated

--- src/components/TableHead.js
+++ src/components/TableHead.js
@@ -33,8 +33,27 @@ class TableHead extends React.Component {
     const { classes, columns, count, options, data, setCellRef, selectedRows } = this.props;
 
     const numSelected = (selectedRows && selectedRows.data.length) || 0;
-    const isDeterminate = numSelected > 0 && numSelected < count;
-    const isChecked = numSelected === count ? true : false;
+    let isDeterminate = numSelected > 0 && numSelected < count;
+    let isChecked = numSelected === count ? true : false;
+
+    // When the disableToolbarSelect option is true, there can be
+    // selected items that aren't visible, so we need to be more
+    // precise when determining if the head checkbox should be checked.
+    if (options.disableToolbarSelect === true) {
+      if (isChecked) {
+        for (let ii = 0; ii < data.length; ii++) {
+          if (!selectedRows.lookup[data[ii].dataIndex]) {
+             isChecked = false;
+             isDeterminate = true;
+            break;
+          }
+        }
+      } else {
+        if (numSelected > count) {
+           isDeterminate = true;
+        }
+      }
+    }
 
     return (
       <MuiTableHead
